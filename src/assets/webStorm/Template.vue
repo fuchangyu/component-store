ce37119cb33a3c523b8a1c15db6560dc7325cbd8
fuchangyu <1334765151@qq.com>
2021-08-21 21:22:32 +0800

    webStorm

---
+++ src/assets/webStorm/Template.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="$NAME">
+    $NAME
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({})
+
+export default class $NAME extends Vue {}
+
+</script>
+
+<style scoped>
+
+</style>
